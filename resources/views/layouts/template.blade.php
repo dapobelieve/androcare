755d49a83bbb7307588d4f99713deb1ab109853c
Oluwatosin <jerexbambex@gmail.com>
2020-08-11 11:10:56 +0100

    Completed the templating

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 	<head>
-		<title>Dr.care - Free Bootstrap 4 Template by Colorlib</title>
+		<title>Androcare</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		@include('includes.head')
