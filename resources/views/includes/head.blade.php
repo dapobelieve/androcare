af2e5214a0ce2ab56625b8687c2a239e86b99c62
Oluwatosin <jerexbambex@gmail.com>
2020-08-11 10:45:25 +0100

    first commit

---
+++ resources/views/includes/head.blade.php
@@ -0,0 +1,13 @@
+<link href="https://fonts.googleapis.com/css?family=Work+Sans:100,200,300,400,500,600,700,800,900" rel="stylesheet">
+<link rel="stylesheet" href="/assets/front/css/open-iconic-bootstrap.min.css">
+<link rel="stylesheet" href="/assets/front/css/animate.css">
+<link rel="stylesheet" href="/assets/front/css/owl.carousel.min.css">
+<link rel="stylesheet" href="/assets/front/css/owl.theme.default.min.css">
+<link rel="stylesheet" href="/assets/front/css/magnific-popup.css">
+<link rel="stylesheet" href="/assets/front/css/aos.css">
+<link rel="stylesheet" href="/assets/front/css/ionicons.min.css">
+<link rel="stylesheet" href="/assets/front/css/bootstrap-datepicker.css">
+<link rel="stylesheet" href="/assets/front/css/jquery.timepicker.css">
+<link rel="stylesheet" href="/assets/front/css/flaticon.css">
+<link rel="stylesheet" href="/assets/front/css/icomoon.css">
+<link rel="stylesheet" href="/assets/front/css/style.css">
---
+++ resources/views/includes/scripts.blade.php
@@ -0,0 +1,27 @@
+<script src="/assets/front/js/jquery.min.js" type="7e700508d0afead9016b28c0-text/javascript"></script>
+<script src="/assets/front/js/jquery-migrate-3.0.1.min.js" type="7e700508d0afead9016b28c0-text/javascript"></script>
+<script src="/assets/front/js/popper.min.js" type="7e700508d0afead9016b28c0-text/javascript"></script>
+<script src="/assets/front/js/bootstrap.min.js" type="7e700508d0afead9016b28c0-text/javascript"></script>
+<script src="/assets/front/js/jquery.easing.1.3.js" type="7e700508d0afead9016b28c0-text/javascript"></script>
+<script src="/assets/front/js/jquery.waypoints.min.js" type="7e700508d0afead9016b28c0-text/javascript"></script>
+<script src="/assets/front/js/jquery.stellar.min.js" type="7e700508d0afead9016b28c0-text/javascript"></script>
+<script src="/assets/front/js/owl.carousel.min.js" type="7e700508d0afead9016b28c0-text/javascript"></script>
+<script src="/assets/front/js/jquery.magnific-popup.min.js" type="7e700508d0afead9016b28c0-text/javascript"></script>
+<script src="/assets/front/js/aos.js" type="7e700508d0afead9016b28c0-text/javascript"></script>
+<script src="/assets/front/js/jquery.animateNumber.min.js" type="7e700508d0afead9016b28c0-text/javascript"></script>
+<script src="/assets/front/js/bootstrap-datepicker.js" type="7e700508d0afead9016b28c0-text/javascript"></script>
+<script src="/assets/front/js/jquery.timepicker.min.js" type="7e700508d0afead9016b28c0-text/javascript"></script>
+<script src="/assets/front/js/scrollax.min.js" type="7e700508d0afead9016b28c0-text/javascript"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&amp;sensor=false" type="7e700508d0afead9016b28c0-text/javascript"></script>
+<script src="/assets/front/js/google-map.js" type="7e700508d0afead9016b28c0-text/javascript"></script>
+<script src="/assets/front/js/main.js" type="7e700508d0afead9016b28c0-text/javascript"></script>
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13" type="7e700508d0afead9016b28c0-text/javascript"></script>
+<script type="7e700508d0afead9016b28c0-text/javascript">
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+<script src="../../../../ajax.cloudflare.com/cdn-cgi/scripts/7089c43e/cloudflare-static/rocket-loader.min.js" data-cf-settings="7e700508d0afead9016b28c0-|49" defer=""></script>
