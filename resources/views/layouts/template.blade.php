72077c37b0e8105ac8044ed07824626305d1a9e8
Oluwatosin <jerexbambex@gmail.com>
2020-08-11 11:09:05 +0100

    Completed the templating

---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+	<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+	<head>
+		<title>Dr.care - Free Bootstrap 4 Template by Colorlib</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		@include('includes.head')
+	</head>
+
+	<body>
+		@include('includes.nav')
+
+		@include('includes.footer')
+		@include('includes.scripts')
+	</body>
+</html>
