88bf19cfa8e3bcda6fbe23776cd4f48b53f11aee
Dapo Michaels <dapomichaels@gmail.com>
2020-08-13 12:35:58 +0100

    docker setup

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
 	<head>
-		<title>Androcare</title>
+		<title>Androcare Fertility Nigeria</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 		@include('includes.head')
